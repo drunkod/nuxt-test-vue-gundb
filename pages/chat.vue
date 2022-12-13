47d1fc8378f7b1ea264ee65774a22c04ce6019f8
From Acer <drunkod@gmail.com>
2022-12-13 17:35:30 +0500

    first commit

---
+++ pages/chat.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ChatRoom } from '@gun-vue/components'
+</script>
+
+<template>
+    <div>
+      ChatRoom
+      <client-only><ChatRoom /></client-only>
+      
+    </div>
+  </template>
